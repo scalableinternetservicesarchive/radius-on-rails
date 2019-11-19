a92d5fd75d126f54e47b7398c807498205d12c70
saiguy3 <saiatmakuri@yahoo.com>
2019-11-19 12:05:13 -0800

    Users can only see their conversations

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,27 +4,31 @@ class MessagesController < ApplicationController
   def index
     # @conversations = Conversation.all
     @conversations = Conversation.where(sender_id: current_user.id).or(Conversation.where(recipient_id: current_user.id))
-    @messages = @conversation.messages
-    if @messages.length > 10
-      @over_ten = true
-      @messages = @messages[-10..-1]
-    end
-    if params[:m]
-      @over_ten = false
-      @messages = @conversation.messages
-    end
-    if @messages.last
-      if @messages.last.user_id != current_user.id
-        @messages.last.read = true
-        @messages.last.save
-      end
-    end
-    @is_blank = @messages.blank?
-    @message = @conversation.messages.new
-    if @conversation.sender_id == current_user.id
-      @recipient = User.find(@conversation.recipient_id)
+    unless @conversations.include?(@conversation)
+      redirect_to conversations_path
     else
-      @recipient = User.find(@conversation.sender_id)
+      @messages = @conversation.messages
+      if @messages.length > 10
+        @over_ten = true
+        @messages = @messages[-10..-1]
+      end
+      if params[:m]
+        @over_ten = false
+        @messages = @conversation.messages
+      end
+      if @messages.last
+        if @messages.last.user_id != current_user.id
+          @messages.last.read = true
+          @messages.last.save
+        end
+      end
+      @is_blank = @messages.blank?
+      @message = @conversation.messages.new
+      if @conversation.sender_id == current_user.id
+        @recipient = User.find(@conversation.recipient_id)
+      else
+        @recipient = User.find(@conversation.sender_id)
+      end
     end
   end
 
